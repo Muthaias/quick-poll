afe3b9e616c6138acf8c36bddb704111de9be5b0
Mattias Nyberg <muthaias.uu@gmail.com>
2020-08-04 23:27:59 +0200

    wip: add initial poll update

--- src/poll.py
+++ src/poll.py
@@ -1,8 +1,22 @@
 from flask import Blueprint, request, abort, jsonify
 from auth import auth
+from uuid import uuid4
 
 poll = Blueprint("poll", __name__)
 polls = []
+
+def _get_options(desc):
+    options = []
+    option = None
+    print(desc)
+    i = 0
+    option = desc.get("option%d" % i)
+    while option:
+        options.append(option)
+        i = i + 1
+        option = desc.get("option%d" % i)
+    print(options)
+    return options
 
 @poll.route("/poll", methods=["POST", "GET"])
 @auth.login_required
@@ -10,34 +24,37 @@ def create_poll():
     owner = auth.current_user()
     desc = request.form if request.form else request.args
     title = desc.get("title")
-    i = 0
-    options = []
-    option = None
-    try:
-        option = desc.get("option%d" % i)
-    except:
-        pass
-    while option:
-        options.append(option)
-        i = i + 1
-        try:
-            option = desc.get("option%d" % i)
-        except:
-            pass
+    options = _get_options(desc)
+
     if owner == None or title == None:
         abort(400)
+
     data = {
-        "id": len(polls),
+        "id": str(uuid4()),
         "owner": owner,
         "title": title,
-        "option": options
+        "options": options
     }
     polls.append(data)
     return jsonify(data)
 
-@poll.route("/poll", methods=["POST", "GET"])
+@poll.route("/poll/<poll_id>", methods=["POST", "GET"])
 @auth.login_required
-def update_poll():
+def update_poll(poll_id: str):
+    print(poll_id, polls)
+    desc = request.form if request.form else request.args
+    title = desc.get("title")
+    options = _get_options(desc)
+    data = next((poll for poll in polls if poll["id"] == poll_id), None)
+
+    if data == None:
+        abort(400)
+
+    data["title"] = title or data["title"]
+    data["options"] = options or data["options"]
+    
+    return jsonify(data)
+
 
 
 @poll.route("/polls", methods=["GET"])
