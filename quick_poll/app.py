fbe22a7949fc663b191a13276f8ab32670d502fa
Mattias Nyberg <muthaias.uu@gmail.com>
2020-08-07 01:09:35 +0200

    wip: silence deprecation warning for sqlalchemy

--- quick_poll/app.py
+++ quick_poll/app.py
@@ -3,4 +3,5 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///quick-poll.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
